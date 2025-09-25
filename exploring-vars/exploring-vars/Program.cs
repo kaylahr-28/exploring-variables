5ac5b33bb7c134295ea02b84a18e315ce411b2eb
kaylahr-28 <kaylahrouzier28@gmail.com>
2025-09-25 09:05:06 -0400

    exercise pt 2 (done)

--- exploring-vars/exploring-vars/Program.cs
+++ exploring-vars/exploring-vars/Program.cs
@@ -44,6 +44,64 @@
 
             //fight
 
+            //evil guys attack hero </3
+            //boss first
+            Console.WriteLine(villainName + " the " + villainTitle + " attacks " + heroFullName);
+            Console.WriteLine(villainName + " the " + villainTitle + " deals " + bossStrength + " damage!");
+            //take current health, subtract damage then store new val back into health
+            heroHealth = heroHealth - bossStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+            //minions attack
+            Console.WriteLine("The minions attack!");
+            Console.WriteLine(minion1Name + " deals " + minionStrength + " damage!");
+            //subtract from hero health
+            heroHealth = heroHealth - minionStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+            Console.WriteLine(minion2Name + " deals " + minionStrength + " damage!");
+            //subtract from hero health
+            heroHealth -= minionStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+
+            //hero attacks
+            Console.WriteLine(heroFullName + " attacks each villain for " + heroStrength + " damage!");
+            //subtract from villains health
+            bossHealth -= heroStrength;
+            minion1Health -= heroStrength;
+            minion2Health -= heroStrength;
+
+            //results in console
+            Console.WriteLine(villainName + " the " + villainTitle + " now has " + bossHealth + " health!");
+            Console.WriteLine(minionTitle + " " + minion1Name + " now has " + minion1Health + " health!");
+            Console.WriteLine(minionTitle + " " + minion2Name + " now has " + minion2Health + " health!");
+
+            //minions are dead
+            Console.WriteLine(minion1Name + " and " + minion2Name + " have been defeated!");
+
+            //end of round buff
+            Console.WriteLine(heroFullName + " has " + heroHealth + " health!");
+            Console.WriteLine(heroFullName + " eats an apple to restore health!");
+            heroHealth++;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+
+
+            //final round
+            Console.WriteLine("The remaining fighters continue the fight!");
+            Console.WriteLine("Hero's health: " + heroHealth);
+            Console.WriteLine("Boss' health: " + bossHealth);
+            //boss attacks first
+            Console.WriteLine(villainName + " the " + villainTitle + " attacks " + heroFullName);
+            Console.WriteLine(villainName + " the " + villainTitle + " deals " + bossStrength + " damage!");
+            heroHealth -= bossStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+            Console.WriteLine(heroFullName + " attacks " + villainName + " the " + villainTitle + " for " + heroStrength + " damage!");
+            //subtract from villains health
+            bossHealth -= heroStrength;
+            Console.WriteLine(villainName + " the " + villainTitle + " now has " + bossHealth + " health!");
+
+            //final results
+            Console.WriteLine(heroFullName + " is victorious!");
+            Console.WriteLine("Wealth, glory, and straight A+'s to the winner!");
+
         }
     }
 }
